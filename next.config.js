1c54b2a5fd08c002589712b4c91db6e3286a47f2
Abhishek Chauhan <ac12644@gmail.com>
2022-07-05 19:11:48 +0200

    added dockerfile

---
+++ next.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    trailingSlash: true,
+  }
+  
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Head from 'next/head';
-
 import Page from '../components/Page';
-
 import 'react-lazy-load-image-component/src/effects/blur.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
@@ -27,7 +25,6 @@ export default function App({ Component, pageProps }) {
   
   );
 }
-
 App.propTypes = {
   Component: PropTypes.elementType.isRequired,
   pageProps: PropTypes.object.isRequired,
