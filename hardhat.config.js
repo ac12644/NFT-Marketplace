6f5157f44668ec6941f11a6403f33fc9af2170db
Abhishek Chauhan <ac12644@Abhisheks-MacBook-Air.local>
2022-08-12 12:10:24 +0200

    bug fix

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,8 +5,6 @@ require("@nomiclabs/hardhat-waffle");
 require("hardhat-gas-reporter");
 require("solidity-coverage");
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
 
@@ -15,12 +13,6 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
   solidity: "0.8.3",
   networks: {
