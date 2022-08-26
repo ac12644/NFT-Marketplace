f9e8ab88a9bf386fd737e78bf9f5c5404cc545f1
Abhishek Chauhan <ac12644@gmail.com>
2022-08-26 13:01:23 +0200

    env added

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 module.exports = {
-    trailingSlash: true,
-  }
-  
+  trailingSlash: true,
+  env: {
+    INFURA_IPFS_ID: process.env.INFURA_IPFS_ID,
+    INFURA_IPFS_SECRET: process.env.INFURA_IPFS_SECRET,
+    PRIVATE_KEY: process.env.PRIVATE_KEY,
+    MUMBAI_URL: process.env.MUMBAI_URL,
+  },
+};
