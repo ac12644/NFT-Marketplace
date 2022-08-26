c52c217352640b18fd36d9666e9d4d57f1074432
Abhishek Chauhan <ac12644@gmail.com>
2022-08-26 13:33:42 +0200

    recompiled contract

--- Address.js
+++ Address.js
@@ -1,2 +1,2 @@
-export const nftAddress = '0x90d319E1416bDfC4eC3e7b9d00646B6F648ed158';
-export const marketAddress = '0xa8b22A1977CB25759a45feAfceE1D1ad74e0f0fa';
+export const nftAddress = '0xE3F0Cf3E667B115043D196424DAb4582B060e2f2';
+export const marketAddress = '0x4f7A6338b6Cc3b83905b29Eb1A8fbADb2c18830E';
