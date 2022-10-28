dc80c4fb1c89ca2b421714bf403d889129b6d758
Abhishek Chauhan <ac12644@gmail.com>
2022-10-28 14:41:49 +0200

    next-config update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  trailingSlash: true,
+  swcMinify: true,
   env: {
+    INFURA_API_KEY: process.env.INFURA_API_KEY,
     INFURA_IPFS_ID: process.env.INFURA_IPFS_ID,
     INFURA_IPFS_SECRET: process.env.INFURA_IPFS_SECRET,
     INFURA_IPFS_DOMAIN: process.env.INFURA_IPFS_DOMAIN,
